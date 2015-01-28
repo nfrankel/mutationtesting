df560363dacbac26d3e4e33d1971f82e16dc58d8
Nicolas Frankel <nicolas@frankel.ch>
2015-01-28 18:17:05 +0100

    Inital commit

---
+++ assertless/src/main/java/ch/frankel/mutationtesting/DuplicateMath.java
@@ -0,0 +1,8 @@
+package ch.frankel.mutationtesting;
+
+public class DuplicateMath {
+
+    public int add(int i1, int i2) {
+        return i1 + i2;
+    }
+}
---
+++ boundary/src/test/java/ch/frankel/mutationtesting/LowPassPredicateTest.java
@@ -0,0 +1,30 @@
+package ch.frankel.mutationtesting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LowPassPredicateTest {
+
+    private static final int LIMIT = 5;
+    private LowPassPredicate predicate;
+
+    @BeforeEach
+    public void setUp() {
+        predicate = new LowPassPredicate(LIMIT);
+    }
+
+    @Test
+    public void should_return_true_when_under_limit() {
+        boolean result = predicate.filter(LIMIT - 1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void should_return_false_when_under_limit() {
+        boolean result = predicate.filter(LIMIT + 1);
+        assertFalse(result);
+    }
+}

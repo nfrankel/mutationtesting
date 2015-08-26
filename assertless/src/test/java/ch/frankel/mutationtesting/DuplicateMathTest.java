479e8192c5f6eefe4889e9ea3faec1ce4ce84df7
Nicolas Frankel <nicolas@frankel.ch>
2015-08-26 20:10:49 +0200

    Fix tests
      Add assertion
      Add boundary test case

--- assertless/src/test/java/ch/frankel/mutationtesting/DuplicateMathTest.java
+++ assertless/src/test/java/ch/frankel/mutationtesting/DuplicateMathTest.java
@@ -2,11 +2,14 @@ package ch.frankel.mutationtesting;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DuplicateMathTest {
 
     @Test
     public void should_return_sum_of_parameters() {
         DuplicateMath math = new DuplicateMath();
         int sum = math.add(1, 1);
+        assertEquals(sum, 2);
     }
 }

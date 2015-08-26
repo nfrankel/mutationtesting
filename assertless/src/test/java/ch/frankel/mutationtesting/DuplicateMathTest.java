c1c65a978a9127a53c263cdce197ec7e17724e17
Nicolas Frankel <nicolas@frankel.ch>
2015-08-26 20:10:49 +0200

    Add assert to test

--- assertless/src/test/java/ch/frankel/mutationtesting/DuplicateMathTest.java
+++ assertless/src/test/java/ch/frankel/mutationtesting/DuplicateMathTest.java
@@ -2,11 +2,14 @@ package ch.frankel.mutationtesting;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class DuplicateMathTest {
 
     @Test
     public void should_return_sum_of_parameters() {
         DuplicateMath math = new DuplicateMath();
         int sum = math.add(1, 1);
+        assertEquals(sum, 2);
     }
 }
